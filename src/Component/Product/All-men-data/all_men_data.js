bbfb903add4a0c4b5a9e821277498def1a4e2db0
Arjun kumar <akarjun1050@gmail.com>
2024-09-01 15:06:48 +0530

    add allen solly product

---
+++ src/Component/Product/All-men-data/all_men_data.js
@@ -0,0 +1,77 @@
+import hoodie from '../../Assets/men/Allen Solly/Hoodie/1.jpeg'
+import jeans from '../../Assets/men/Allen Solly/Jeans/1.jpeg'
+import shirt from '../../Assets/men/Allen Solly/Shirt/1.jpeg'
+import pant from '../../Assets/men/Allen Solly/Pant/1.jpeg'
+import lower from '../../Assets/men/Allen Solly/Lower/1.jpeg'
+import t_shirt from '../../Assets/men/Allen Solly/T-shirt/1.jpeg'
+
+let allen_solly=[
+    {
+        id:1,
+        brand:"Allen Solly",
+        name:"Hoodie",
+        image:hoodie,
+        new_price:"₹1379",
+        old_price:"₹2299",
+        offer:"40% off",
+        rating:"4.3★",
+        review:1256, 
+    },
+    {
+        id:2,
+        brand:"Allen Solly",
+        name:"Jeans",
+        image:jeans,
+        new_price:"₹1379",
+        old_price:"₹2299",
+        offer:"40% off",
+        rating:"4.3★",
+        review:1256, 
+    },
+    {
+        id:3,
+        brand:"Allen Solly",
+        name:"Shirt",
+        image:shirt,
+        new_price:"₹1379",
+        old_price:"₹2299",
+        offer:"40% off",
+        rating:"4.3★",
+        review:1256, 
+    },
+    {
+        id:4,
+        brand:"Allen Solly",
+        name:"Pant",
+        image:pant,
+        new_price:"₹1379",
+        old_price:"₹2299",
+        offer:"40% off",
+        rating:"4.3★",
+        review:1256, 
+    },
+    {
+        id:5,
+        brand:"Allen Solly",
+        name:"Lower",
+        image:lower,
+        new_price:"₹1379",
+        old_price:"₹2299",
+        offer:"40% off",
+        rating:"4.3★",
+        review:1256, 
+    },
+    {
+        id:6,
+        brand:"Allen Solly",
+        name:"T-shirt",
+        image:t_shirt,
+        new_price:"₹1379",
+        old_price:"₹2299",
+        offer:"40% off",
+        rating:"4.3★",
+        review:1256, 
+    }
+]
+
+export default allen_solly;
---
+++ src/Pages/Men.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import allen_solly from '../Component/Product/All-men-data/all_men_data';
+
+function Men(props) {
+    return (
+        <>
+        <h1 className='text-4xl font-bold my-3 text-center md:text-6xl'>Shop by Brand</h1>
+        <hr className='w-full mt-1' />
+        <div className="flex justify-between items-center px-4">
+            <h4 className='font-bold text-xl my-[6px]' onClick={()=>{redirect('/realme')}} >Allen Solly</h4>
+            <Link to='/allen_solly' className=''>View all</Link>
+            </div>
+                {/* Realme */}
+            <hr className='w-full' />
+            <div className="flex justify-around overflow-x-scroll webkit w-full my-[10px]">
+            {allen_solly.map((val,i)=>
+              <div className="flex flex-col items-center gap-2 w-36 sm:w-40 md:w-44 px-3 " key={i}>
+                <img src={val.image} alt="" className='max-w-full h-auto' />
+               <div className="flex flex-col items-center">
+                <div className="text-sm font-bold">{val.name}</div>
+                <div className="text-sm">({val.detail})</div>
+               </div>
+                <div className="flex gap-2">
+                    <div className="text-sm px-1 bg-green-700 text-white">{val.rating}☆</div>
+                    <div className="text-xs">({val.review})</div>
+                </div>
+                <div className="flex gap-3">
+                    <div className="text-sm">{val.new_price}</div>
+                    <div className="text-sm line-through text-gray-600">{val.old_price}</div>
+                </div>
+              </div>
+            )}
+          </div>
+        </>
+    );
+}
+
+export default Men;
