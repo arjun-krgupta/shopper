770039acfd83ed2da64f7b0c3e4d9d606994dbe6
Arjun kumar <akarjun1050@gmail.com>
2024-09-01 17:00:27 +0530

    update men.jsx

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -20,8 +20,8 @@ function Men(props) {
               <div className="flex flex-col items-center gap-2 w-36 sm:w-40 md:w-44 px-3 " key={i}>
                 <img src={val.image} alt="" className='max-w-full h-auto' />
                <div className="flex flex-col items-center">
-                <div className="text-sm font-bold">{val.name}</div>
-                <div className="text-sm">({val.detail})</div>
+                <div className="text-sm text-gray-400">{val.brand}</div>
+                <div className="text-sm">({val.name})</div>
                </div>
                 <div className="flex gap-2">
                     <div className="text-sm px-1 bg-green-700 text-white">{val.rating}☆</div>
