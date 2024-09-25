6c03427aafc27531b26dd185865b4de262057126
Arjun kumar <akarjun1050@gmail.com>
2024-09-25 11:58:21 +0530

    modify the samsung page

--- src/Pages/Mobile/Samsung.jsx
+++ src/Pages/Mobile/Samsung.jsx
@@ -42,7 +42,7 @@ function Samsung(props) {
         <div className='mt-2'>
             {
                 Samsung_product.map((val,ind)=>
-             <div className="flex justify-evenly bg-white p-3 pb-3 border-bottom mb-2 w-full cursor-pointer" key={ind}>
+             <div className="flex justify-around bg-white p-3 pb-3 border-bottom mb-2 w-full cursor-pointer" key={ind}>
             <div className="w-[16%] md:w-[20%] mt-2">
                 <img src={val.image} alt="" className='max-w-full h-auto'/>
             </div>
