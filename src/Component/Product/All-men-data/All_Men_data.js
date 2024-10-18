b7e7fd922059432fc7be2f255dcc3e6995f9dfd9
Arjun kumar <akarjun1050@gmail.com>
2024-10-18 18:24:01 +0530

    update the men file

--- src/Component/Product/All-men-data/All_Men_data.js
+++ src/Component/Product/All-men-data/All_Men_data.js
@@ -3,42 +3,37 @@ import h2 from '../../Assets/men/Cobb/Hoodie/2.jpeg'
 import h3 from '../../Assets/men/Cobb/Hoodie/3.jpeg'
 import h4 from '../../Assets/men/Cobb/Hoodie/4.jpeg'
 import h5 from '../../Assets/men/Cobb/Hoodie/5.jpeg'
-import h6 from '../../Assets/men/Cobb/Hoodie/6.jpeg'
 
 import j1 from '../../Assets/men/Cobb/Jeans/1.jpeg'
 import j2 from '../../Assets/men/Cobb/Jeans/2.jpeg'
 import j3 from '../../Assets/men/Cobb/Jeans/3.jpeg'
 import j4 from '../../Assets/men/Cobb/Jeans/4.jpeg'
 import j5 from '../../Assets/men/Cobb/Jeans/5.jpeg'
-import j6 from '../../Assets/men/Cobb/Jeans/6.jpeg'
 
 import p1 from '../../Assets/men/Cobb/Pant/1.jpg'
 import p2 from '../../Assets/men/Cobb/Pant/2.jpg'
 import p3 from '../../Assets/men/Cobb/Pant/3.jpg'
 import p4 from '../../Assets/men/Cobb/Pant/4.jpg'
 import p5 from '../../Assets/men/Cobb/Pant/5.jpg'
-import p6 from '../../Assets/men/Cobb/Pant/6.jpg'
 
 import l1 from '../../Assets/men/Cobb/Lower/1.jpeg'
 import l2 from '../../Assets/men/Cobb/Lower/2.jpeg'
 import l3 from '../../Assets/men/Cobb/Lower/3.jpeg'
 import l4 from '../../Assets/men/Cobb/Lower/4.jpeg'
 import l5 from '../../Assets/men/Cobb/Lower/5.jpeg'
-import l6 from '../../Assets/men/Cobb/Lower/6.jpeg'
 
 import s1 from '../../Assets/men/Cobb/Shirt/1.jpg'
 import s2 from '../../Assets/men/Cobb/Shirt/2.jpg'
 import s3 from '../../Assets/men/Cobb/Shirt/3.jpg'
 import s4 from '../../Assets/men/Cobb/Shirt/4.jpg'
 import s5 from '../../Assets/men/Cobb/Shirt/5.jpg'
-import s6 from '../../Assets/men/Cobb/Shirt/6.jpg'
+
 
 import ts1 from '../../Assets/men/Cobb/T-shirt/1.jpeg'
 import ts2 from '../../Assets/men/Cobb/T-shirt/2.jpeg'
 import ts3 from '../../Assets/men/Cobb/T-shirt/3.jpeg'
 import ts4 from '../../Assets/men/Cobb/T-shirt/4.jpeg'
 import ts5 from '../../Assets/men/Cobb/T-shirt/5.jpeg'
-import ts6 from '../../Assets/men/Cobb/T-shirt/6.jpeg'
 
 let hoodie=[
     {
@@ -101,18 +96,6 @@ let hoodie=[
         review:1256, 
         size:"M , L , XL",
     },
-    {
-        id:6,
-        brand:"Cobb Italy",
-        name:"Hoodie",
-        image:h6,
-        new_price:"₹1379",
-        old_price:"₹2299",
-        offer:"40% off",
-        rating:"4.3★",
-        review:1256, 
-        size:"M , L , XL",
-    }
 ]
 export {hoodie}
 
@@ -177,18 +160,6 @@ let jeans=[
         review:1256, 
         size:"30 , 32 , 34 , 36",
     },
-    {
-        id:6,
-        brand:"Cobb Italy",
-        name:"Jeans",
-        image:j6,
-        new_price:"₹1379",
-        old_price:"₹2299",
-        offer:"40% off",
-        rating:"4.3★",
-        review:1256, 
-        size:"30 , 32 , 34 , 36",
-    }
 ]
 export {jeans}
 
@@ -253,18 +224,6 @@ let pant=[
         review:1256, 
         size:"30 , 32 , 34 , 36",
     },
-    {
-        id:6,
-        brand:"Cobb Italy",
-        name:"Pant",
-        image:p6,
-        new_price:"₹1379",
-        old_price:"₹2299",
-        offer:"40% off",
-        rating:"4.3★",
-        review:1256, 
-        size:"30 , 32 , 34 , 36",
-    }
 ]
 export {pant}
 
@@ -329,18 +288,6 @@ let lower=[
         review:1256,
         size:"30 , 32 , 34 , 36",
     },
-    {
-        id:6,
-        brand:"Cobb Italy",
-        name:"Track Pants",
-        image:l6,
-        new_price:"₹1379",
-        old_price:"₹2299",
-        offer:"40% off",
-        rating:"4.3★",
-        review:1256, 
-        size:"30 , 32 , 34 , 36",
-    }
 ]
 export {lower}
 
@@ -405,18 +352,6 @@ let shirt=[
         review:1256, 
         size:"M , L , XL",
     },
-    {
-        id:6,
-        brand:"Cobb Italy",
-        name:"Shirt",
-        image:s6,
-        new_price:"₹1379",
-        old_price:"₹2299",
-        offer:"40% off",
-        rating:"4.3★",
-        review:1256, 
-        size:"M , L , XL",
-    }
 ]
 export {shirt}
 
@@ -481,17 +416,5 @@ let t_shirt_men=[
         review:1256, 
         size:"M , L , XL",
     },
-    {
-        id:6,
-        brand:"Cobb Italy",
-        name:"T-Shirt",
-        image:ts6,
-        new_price:"₹1379",
-        old_price:"₹2299",
-        offer:"40% off",
-        rating:"4.3★",
-        review:1256, 
-        size:"M , L , XL",
-    }
 ]
 export {t_shirt_men}
